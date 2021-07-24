b1a48c33a593664778d8e02f0ef95a9a7fa4f91f
wlad-kryhin <75882471+wlad-kryhin@users.noreply.github.com>
2021-07-24 13:02:16 +0000

    Deploying to gh-pages from @ wlad-kryhin/project-JS-NightCoders@baf7477f82dace430415d92d59b3de66232ee2cf 🚀

--- src.e53117ff.js
+++ src.e53117ff.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-JS-NightCoders/src.e4d5bf0f.js.map
+//# sourceMappingURL=/project-JS-NightCoders/src.e53117ff.js.map
