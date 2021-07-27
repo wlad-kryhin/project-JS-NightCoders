623ea863374f92de71cfaa8022f093b06328b341
Wlad <wladkrychin@gmail.com>
2021-07-27 18:59:39 +0200

    change theme

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,7 +1,9 @@
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
-  };
+};
+const section = document.querySelector('.section-hero')
+
 
   const checkBox = document.querySelector('#theme-switch-toggle')
   checkBox.addEventListener('change', onChangeInput)
@@ -11,15 +13,15 @@ const Theme = {
   }
   
   function onCheckedInput() {
-    document.body.classList.add(Theme.DARK);
-    document.body.classList.remove(Theme.LIGHT);
+    section.classList.add(Theme.DARK);
+    section.classList.remove(Theme.LIGHT);
     localStorage.setItem('theme', Theme.DARK);
     checkBox.checked = true;
   }
   
   function offCheckedInput() {
-    document.body.classList.add(Theme.LIGHT);
-    document.body.classList.remove(Theme.DARK);
+    section.classList.add(Theme.LIGHT);
+    section.classList.remove(Theme.DARK);
     localStorage.setItem('theme', Theme.LIGHT);
     checkBox.checked = false;
   }
