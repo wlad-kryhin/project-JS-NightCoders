6fa123306241a7c8529c50ef53eb231407d273b4
Wlad <wladkrychin@gmail.com>
2021-07-29 18:52:59 +0200

    add ID for cards

--- src/js/myLibraryCards.js
+++ src/js/myLibraryCards.js
@@ -11,7 +11,7 @@ const localeStorageAPI = new LocaleStorageAPI();
       .then(r => r.json())
       .then(data => ({
         ...data,
-        release_date: data.release_date.split('-')[0],
+        // release_date: data.release_date.split('-')[0],
       }));
   });
 
@@ -26,5 +26,20 @@ export default function renderFilmsLibrary() {
   }
 
 function libraryCardsMarkup(data) {
- return  container.innerHTML = cardsTemplate(data)
+  return container.innerHTML = cardsTemplate(data)
+  //  return container.insertAdjacentHTML('beforeend', markupLibraryHtml(data))
 }
+// function markupLibraryHtml(movie) {
+//   const { poster_path, original_title, release_date, genre_ids, vote_average, id } = movie;
+//       const movieItem = `<li class="film-item">
+//                   <a href="" class="film-link">
+//                   <img src="https://image.tmdb.org/t/p/w342/${poster_path}" alt="{{original_title}}" class="film-img" id="${id}">
+//                   <p class="film-description">${original_title}
+//                   </p>
+//                   <p class="film-description  film-gengers">${makeStringOfGenres(genre_ids)},       
+//           Other | ${release_date.slice(0, 4)} <span class="film-raiting">${vote_average}<span>
+//                   </p>
+//                   </a>
+//                   </li>`;
+// return movieItem.join('')
+// }
