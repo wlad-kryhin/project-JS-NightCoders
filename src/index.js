b798bf74dc135bac7b834b7fd6eec9a868acecc8
KasyanAndrey <andreykasyan88@gmail.com>
2021-08-03 07:15:12 +0300

    fixed file munu-filter.js/html/scss and add files release-date.json/genresList.hbs/releaseDate.hbs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './sass/main.scss';
-// import './js/menu-filter.js';
+import './js/menu-filter.js';
 import Spinner from './js/spinner'; // import Spinner(active , hidden)
 import LocaleStorageAPI from './js/localStorageAPI';
 import './js/clock';
@@ -17,8 +17,7 @@ import './js/library-background';
 import getMovies from './js/myLibraryCards';
 import renderFilmsLibrary from './js/myLibraryCards';
 import './js/slider';
-// showMenuFilter();
-import { currentThemeWebSite} from './js/toggle';
+import { currentThemeWebSite } from './js/toggle';
 const refs = {
   slider: document.querySelector('.slider-wrapper'),
   searchForm: document.querySelector('[data-index="search-form"]'),
@@ -89,7 +88,7 @@ function myLibraryPageChange() {
 refsHeader.myLibraryBtn.addEventListener('click', e => {
   e.preventDefault();
   myLibraryPageChange();
-  currentThemeWebSite()
+  currentThemeWebSite();
   refsHeader.myLibraryBtn.classList.add('current');
   refsHeader.homeBtn.classList.remove('current');
   refs.searchForm.innerHTML = `<button class="library-button active-btn" data-action="show-watched">
@@ -99,7 +98,7 @@ refsHeader.myLibraryBtn.addEventListener('click', e => {
                     QUEUE
                 </button>`;
   refs.filmsContainer.innerHTML = '';
-  refs.slider.style.display = 'none'
+  refs.slider.style.display = 'none';
   renderFilmsLibrary(localeStorageAPI.getValueWatched());
   const refsShow = {
     showWatchedBtn: document.querySelector('[data-action="show-watched"]'),
--- src/js/menu-filter.js
+++ src/js/menu-filter.js
@@ -1,31 +1,76 @@
-// const API_KEY = 'f67bdd430ce819844e2a075541409928';
-// const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'f67bdd430ce819844e2a075541409928';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
+import genresListTpl from '../templates/genresList.hbs';
+import releaseDateTpl from '../templates/releaseDateList';
+import date from '../json/release-date.json';
 
 const refs = {
   buttonOpenMenuEl: document.querySelector('[data-action="open-menu-filter"]'),
   filterListEl: document.querySelector('.filter-list'),
   filterListCloseEl: document.querySelector('.filter-list__button-close'),
+
   itemGenresEl: document.querySelector('.item-genres'),
   listGenresEl: document.querySelector('.list-genres'),
-  genresCloseEl: document.querySelector('[data-action="genres-button-close"]'),
-  dateItemList: document.querySelector('.list-date'),
+  genresCloseEl: document.querySelector('[data-action="close-button-genres"]'),
+  genresLabelEl: document.querySelectorAll('.films-genres__label'),
+
+  ItemDateList: document.querySelector('.item-date'),
+  listDateEl: document.querySelector('.list-date'),
   dateCloseEl: document.querySelector('[data-action="date-button-close"]'),
 };
+
+// ==================== addEventListener ======================
 
 refs.buttonOpenMenuEl.addEventListener('click', showMenuFilter);
 refs.filterListCloseEl.addEventListener('click', onCloseMenuFilter);
 
-refs.itemGenresEl.addEventListener('click', onOpenGenres);
+refs.itemGenresEl.addEventListener('click', showMenuGenres);
 refs.genresCloseEl.addEventListener('click', onCloseGenres);
 
-refs.dateItemList.addEventListener('click', onOpenDate);
+refs.ItemDateList.addEventListener('click', showMenuReleaseDate);
 refs.dateCloseEl.addEventListener('click', onCloseDate);
+
+// ================= MenuFilter ===================
 
 function showMenuFilter(e) {
   e.preventDefault();
+  const target = e.target;
 
   onOpenMenuFilter();
 }
+
+// ==================== MenuGenres / MenuReleaseDate================
+
+function showMenuGenres(e) {
+  onOpenGenres();
+  fetchGenres(e.target.genres).then(renderGenresList);
+}
+
+function showMenuReleaseDate(e) {
+  onOpenDate();
+  renderReleaseDateLis(date);
+}
+
+// ===================== API ==========================
+
+function fetchGenres(genre) {
+  return fetch(`${BASE_URL}genre/movie/list?api_key=${API_KEY}&language=en-US`).then(response => {
+    return response.json();
+  });
+}
+
+// ==================== render ===========================
+
+function renderGenresList(genre) {
+  refs.listGenresEl.insertAdjacentHTML('beforeend', genresListTpl(genre));
+}
+
+function renderReleaseDateLis(date) {
+  refs.listDateEl.insertAdjacentHTML('beforeend', releaseDateTpl(date));
+}
+
+// =================== open / close============================
 
 function onOpenMenuFilter() {
   refs.filterListEl.classList.remove('is-hidden');
@@ -41,12 +86,18 @@ function onOpenGenres() {
   refs.listGenresEl.classList.remove('is-hidden');
 }
 
-// function onOpenDate() {
-//     refs.dateItemList.classList.add('is-hidden');
-//   }
+function onCloseGenres(e) {
+  e.stopPropagation();
+  refs.listGenresEl.classList.add('is-hidden');
+}
 
-//   function onCloseDate() {
-//     refs.listGenresEl.classList.remove('is-hidden');
-//   }
+function onOpenDate() {
+  refs.listDateEl.classList.remove('is-hidden');
+}
 
-export default showMenuFilter;
+function onCloseDate(e) {
+  e.stopPropagation();
+  refs.listDateEl.classList.add('is-hidden');
+}
+
+function resetList() {}
