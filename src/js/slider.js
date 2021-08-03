faeb9a543d86d689902dff1415ff817f26bc7cd3
Wlad <wladkrychin@gmail.com>
2021-08-03 13:32:30 +0200

    fixed slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,7 +1,7 @@
 import Glide from '@glidejs/glide';
 import filmsCardSliderTpl from '../templates/card-films-slider.hbs';
 const sliderContainer = document.querySelector('.js-slider-container');
-const sliderWrapper = document.querySelector('.slider-wrapper');
+// const sliderWrapper = document.querySelector('.slider-wrapper');
 
 renderTrendy();
 
@@ -17,7 +17,7 @@ const glide = new Glide('.glide', {
 glide.mount();
 
 function renderTrendy() {
-  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=92ffb34e08e714eb390805a25b0a06d3`;
+  const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=92ffb34e08e714eb390805a25b0a06d3`;
   return fetch(url)
     .then(response => response.json())
     .then(({ results }) => {
@@ -25,7 +25,7 @@ function renderTrendy() {
     })
     .then(renderSliderFilms)
     .catch(err => {
-      // sliderContainer.innerHTML = `${errorUrl}`;
+      // sliderContainer.innerHTML = `err`;
     });
 }
 
@@ -33,9 +33,3 @@ function renderSliderFilms(articles) {
   sliderContainer.innerHTML = filmsCardSliderTpl(articles);
   trailer.createTrailerLink(document.querySelectorAll('.btn-youtube-slider'));
 }
-
-function clearSlider() {
-  sliderWrapper.innerHTML = '';
-}
-
-export { clearSlider };
