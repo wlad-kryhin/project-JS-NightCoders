20c7d7e3dcf2386250b5376a9d1b0897f13b75df
Wlad <wladkrychin@gmail.com>
2021-10-27 20:13:01 +0200

    Update toggle.js

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,43 +1,42 @@
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
 };
-const section = document.querySelector('.section-hero')
+const section = document.querySelector('.section-hero');
 
+const checkBox = document.querySelector('#theme-switch-toggle');
+checkBox.addEventListener('change', onChangeInput);
 
-  const checkBox = document.querySelector('#theme-switch-toggle')
-  checkBox.addEventListener('change', onChangeInput)
-   
-  function onChangeInput(event) {
-    event.currentTarget.checked ? onCheckedInput() : offCheckedInput();
+function onChangeInput(event) {
+  event.currentTarget.checked ? onCheckedInput() : offCheckedInput();
+}
+
+function onCheckedInput() {
+  section.classList.add(Theme.DARK);
+  section.classList.remove(Theme.LIGHT);
+  localStorage.setItem('theme', Theme.DARK);
+  checkBox.checked = true;
+}
+
+function offCheckedInput() {
+  section.classList.add(Theme.LIGHT);
+  section.classList.remove(Theme.DARK);
+  localStorage.setItem('theme', Theme.LIGHT);
+  checkBox.checked = false;
+}
+
+function currentThemeWebSite() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === Theme.LIGHT || savedTheme === null) {
+    offCheckedInput();
+    return;
   }
-  
-  function onCheckedInput() {
-    section.classList.add(Theme.DARK);
-    section.classList.remove(Theme.LIGHT);
-    localStorage.setItem('theme', Theme.DARK);
-    checkBox.checked = true;
+
+  if (savedTheme === Theme.DARK) {
+    onCheckedInput();
+    return;
   }
-  
-  function offCheckedInput() {
-    section.classList.add(Theme.LIGHT);
-    section.classList.remove(Theme.DARK);
-    localStorage.setItem('theme', Theme.LIGHT);
-    checkBox.checked = false;
-  }
-  
-  function currentThemeWebSite() {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === Theme.LIGHT || savedTheme === null) {
-      offCheckedInput();
-      return;
-    }
-  
-    if (savedTheme === Theme.DARK) {
-      onCheckedInput();
-      return;
-    }
-  }
-  
+}
+
 currentThemeWebSite();
-export {currentThemeWebSite}
+export { currentThemeWebSite };
